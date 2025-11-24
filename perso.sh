2c04e84b40046a497c2b6222df2049ec914c9a90
aguy tech <aguytech@free.fr>
2025-11-24 09:45:00 +0100

    perso.sh: change script to launch perso part from perso.sh

---
+++ perso.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+######################## CONF
+
+S_TRACE=debug
+_PATH_BASE=$( readlink -f ${0%/*} )
+_PATH_CONF=/usr/local/conf
+_PATH_LOG=/var/log/desktop-install
+_CMD="sudo pacman --noconfirm"
+_CMD_INS="sudo pacman --noconfirm --needed -S"
+_CMD_AUR="yay -S --noconfirm --needed"
+_path_base=$( readlink -f ${0%/*}/../ )
+
+# inc
+file=${_path_base}/bs/inc
+! [ -f "${file}" ] && echo "Unable to find file: ${file}" && exit 1
+! . ${file} && echo "Errors while sourcing file: ${file}" && exit 1
+
+########################  SUB
+
+subpart=perso
+parts_perso="global conf root mozilla"
+# sublimetext sublimemerge
+
+for _PART in ${parts_perso}; do
+	_source_sub "${_PART}" ${subpart}
+done
+
+_exit
